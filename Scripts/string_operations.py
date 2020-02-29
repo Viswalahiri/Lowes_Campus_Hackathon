8e16a3daaa4fb8f452d77bc13f6d6a69a8cc6dc4
viswalahiri <viswalahiriswamyh@gmail.com>
2020-02-29 10:20:55 +0530

    ⌛ Refactored and enforced standards

--- Scripts/string_operations.py
+++ Scripts/string_operations.py
@@ -1,30 +1,41 @@
-import re
-import os
-import sqlite3
-import pickle
-import nltk
-from nltk.corpus import wordnet 
-from nltk.stem import WordNetLemmatizer 
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""string_operations.py: Contains core NLP Operations of API service."""
+
+__author__ = "Viswalahiri Swamy Hejeebu"
+__credits__ = ["Viswalahiri Swamy Hejeebu"]
+__license__ = "MIT"
+__version__ = "0.1.0"
+__maintainer__ = "Viswalahiri Swamy Hejeebu"
+__email__ = 'viswalahiriswamyh@gmail.com'
+__status__ = 'Dev'
+
+
 import importlib
-# from hash_map_operations import initial_data_dump,read_hash_map,create_hash_map,update_hash_map,access_hash_map
-# from database_access import initial_data_dump_in_db,insert_to_db
+import re
+
+import nltk
+from nltk.corpus import wordnet
+from nltk.stem import WordNetLemmatizer
+
 
 def all_syno(word):
-    word = '_'.join(word.split(" "))
-    all_syn = []
-    lemmatizer = WordNetLemmatizer()
-    lemmatized_input = lemmatizer.lemmatize(word)
-    syns = wordnet.synsets(lemmatized_input,)   
-    for i in range(len(syns)):
-        lemmas = syns[i].lemmas()
-        for i in lemmas:
-            if (i.name()) not in all_syn:
-                all_syn.append('_'.join(i.name().lower().split('-')))
-    # print(all_syn)
-    return(all_syn)
+	word = '_'.join(word.split(" "))
+	all_syn = []
+	lemmatizer = WordNetLemmatizer()
+	lemmatized_input = lemmatizer.lemmatize(word)
+	syns = wordnet.synsets(lemmatized_input,)   
+	for i in range(len(syns)):
+		lemmas = syns[i].lemmas()
+		for i in lemmas:
+			if (i.name()) not in all_syn:
+				all_syn.append('_'.join(i.name().lower().split('-')))
+	return(all_syn)
+
 def parse_string(string):
-	b= []
-	c= []
+	b = []
+	c = []
 	a = re.findall(r"[\w']+", string)
 	for i in range(len(a)-1):
 		if(a[i].isdigit() and a[i+1].isdigit()):
@@ -46,10 +57,10 @@ def parse_string(string):
 				c.append(i)
 	b = b[:-1]
 	b.extend(c)
-	# print(b)
 	return b
+
 def file_len(fname):
-    with open(fname) as f:
-        for i, l in enumerate(f):
-            pass
-    return i + 1
+	with open(fname) as f:
+		for i, l in enumerate(f):
+			pass
+	return i + 1
