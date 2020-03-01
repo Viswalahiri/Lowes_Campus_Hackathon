7d9b0556a4441f9032f78695169e8c2658a69693
viswalahiri <viswalahiriswamyh@gmail.com>
2020-03-01 22:26:36 +0530

    🐍 Updated app.py

--- Scripts/app.py
+++ Scripts/app.py
@@ -14,6 +14,8 @@ __status__ = 'Dev'
 
 import importlib
 import json
+import os
+from os import path
 import sqlite3
 import subprocess
 
@@ -33,8 +35,10 @@ def home():
 
 @app.route("/update_db",methods = ['POST'])
 def update_db():
-	website_link, product_number, model_number, star_rating, recommendation_chance,asset_link,brand_name, product_description, was_price, is_price = [str(x) for x in request.form.values()]
-	conn = sqlite3.connect('database/inventory.db')
+	website_link, product_number, model_number, star_rating, recommendation_chance,asset_link,brand_name, product_description, was_price, is_price, dummy = [str(x) for x in request.form.values()]
+	THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+	inventory_ = os.path.join(THIS_FOLDER, 'inventory.db')
+	conn = sqlite3.connect(inventory_)
 	c = conn.cursor()
 	c.execute("SELECT product_number FROM inventory WHERE product_number = ?",(product_number,))
 	try:
@@ -44,30 +48,36 @@ def update_db():
 			dynamic_data_dump(product_number,product_description)
 			return render_template('index.html', confirmation_text = 'Details have been uploaded.')
 		else:
-			return render_template('index.html', confirmation_text = 'Duplicate detected.')		
+			return render_template('index.html', confirmation_text = 'Duplicate detected.')
 	except:
-		return render_template('index.html', confirmation_text = 'Error.')				
+		return render_template('index.html', confirmation_text = 'Error.')
 		pass
 
 
 @app.route("/general_search/<string:n_query>",methods = ['GET'])
 def general_search(n_query):
 	query = ' '.join(n_query.split('%'))
-	conn = sqlite3.connect('database/inventory.db')
+	THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+	inventory_ = os.path.join(THIS_FOLDER, 'inventory.db')
+	conn = sqlite3.connect(inventory_)
 	c = conn.cursor()
 	m = []
 	for i in access_hash_map(query):
 		c.execute(" SELECT website_link,asset_link,product_description,is_price,star_rating FROM inventory WHERE product_number = {0}".format(i[1]))
 		m.append(c.fetchall())
-	main_json = {}
+	main_json = []
 	count = 0
 	for i in m:
 		for j in i:
 			my_json_string = {'website_link': j[0], 'asset_link': j[1],'product_description':j[2], 'now_price':str(int(j[3])), 'ratings':str(j[4])}
 			count += 1
-		main_json['Product_'+str(count)] = my_json_string
+			main_json.append(my_json_string)
 	serialized = json.dumps(main_json, sort_keys = True, indent = 3)
 	return serialized
+
+@app.route("/demo")
+def demo():
+	return render_template('trial_indian_web.html')
 
 if __name__ == "__main__":
     app.run(debug = True)
